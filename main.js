664bd29ea85b860cae9e62a0964639da0a3bd10d
赵之琛 <zhaozc@shanshan-business.com>
2022-08-15 21:37:58 +0800

    除第十关外做完了

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const { app, BrowserWindow, Menu, Tray, dialog, ipcMain, clipboard } = require('electron')
+const { app, BrowserWindow, Menu, Tray, dialog, ipcMain, clipboard, shell } = require('electron')
 const path = require("path")
 
+let tray = null;// 设置系统托盘
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     title: "寻找小希",
     webPreferences: {
-      preload: path.join(__dirname,"preload.js")
-    }
+      preload: path.join(__dirname, "preload.js")
+    },
+    icon: path.join(__dirname, "./xiaoxi/love.png")
   })
 
   win.loadFile('./html/index.html')
@@ -22,62 +24,141 @@ const createWindow = () => {
         const blur5seconds = () => {
           timeoutId = setTimeout(() => {
             win.webContents.send("success", "ok");
-            win.webContents.off("blur",blur5seconds);
+            win.webContents.off("blur", blur5seconds);
             timeoutId = null;
-          },5000)
+          }, 5000)
         }
-        win.webContents.on("blur",blur5seconds);
+        win.webContents.on("blur", blur5seconds);
         win.webContents.on("focus", () => {
           if (timeoutId) clearTimeout(timeoutId);
         })
         break;
       case 6:
-        ipcMain.handle("getClipBoard",() => clipboard.readText())
+        ipcMain.handle("getClipBoard", () => clipboard.readText())
         break;
       case 7:
-        
+        // 菜单栏模板
+        const menuBar = [
+          {
+            label: '这是啥',
+            submenu: [
+              {
+                label: '这是啥', click: () => {
+                  dialog.showMessageBoxSync({
+                    "title": "寻找小希",
+                    "message": "这是一个寻找小希的小游戏，可以跟我一起大喊小希生日快乐吗？",
+                    "icon": path.join(__dirname,'./xiaoxi/love.png')
+                  })
+                }
+              },
+              { label: '退出', role: 'quit' }
+            ]
+          },
+          {
+            label: '我是谁',
+            submenu: [
+              {
+                label: '关于小希', click: () => {
+                  shell.openExternal("https://zh.moegirl.org.cn/zh/%E5%B0%8F%E5%B8%8C%E5%B0%8F%E6%A1%83")
+                }
+              },
+              {
+                label: '虚拟次元计划官网', click: () => {
+                  shell.openExternal("https://www.vdproject.cn/")
+                }
+              },
+              { type: 'separator' },
+              {
+                label: '该项目开源地址', click: () => {
+                  shell.openExternal("https://github.com/zhzhch335/findxiaoxi")
+                }
+              },
+              { type: 'separator' },
+              {
+                label: '希爹在此！',
+                sublabel: '小希，找到你了！',
+                icon: './xiaoxi/alert.jpg',
+                click: () => {
+                  win.webContents.send("success", "ok");
+                  initMenu();
+                }
+              }
+            ]
+          }
+        ];
+
+        // 构建菜单项
+        const menu = Menu.buildFromTemplate(menuBar);
+        // 设置一个顶部菜单栏
+        Menu.setApplicationMenu(menu);
         break;
+      case 8:
+        tray = new Tray('./xiaoxi/love.png')
+        tray.setToolTip('小希，找到你了！')
+        tray.on('click', () => {
+          win.webContents.send("success", "ok");
+          tray.destroy();
+        })
+        break;
+      case 9:
+        win.on('close', (event) => {
+          dialog.showMessageBoxSync({
+            "title": "错了错了",
+            "message": "希爹出现了，不要关闭游戏好不好~",
+            icon: "./xiaoxi/love.png"
+          })
+          win.webContents.send("success", "ok");
+          event.preventDefault();
+        });
     }
   })
 
-  // 阻止窗口关闭
-  // win.on('close', (event) => {
-  //   console.log("错了错了！")
-  //   event.preventDefault();
-  // });
 }
 
-let tray = null
 app.whenReady().then(() => {
 
   createWindow()
+  initMenu();
 
-  tray = new Tray('./xiaoxi/0.png')
-  const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item1', type: 'radio' },
-    { label: 'Item2', type: 'radio' },
-    { label: 'Item3', type: 'radio', checked: true },
-    { label: 'Item4', type: 'radio' }
-  ])
-  tray.setToolTip('小希，找到你了！')
-  tray.setContextMenu(contextMenu)
+})
 
-
+function initMenu() {
   // 菜单栏模板
   const menuBar = [
     {
       label: '这是啥',
       submenu: [
-        { label: '这是啥'},
-        
-        { label: '退出' }
+        {
+          label: '这是啥', click: () => {
+            dialog.showMessageBoxSync({
+              "title": "寻找小希",
+              "message": "这是一个寻找小希的小游戏，可以跟我一起大喊“小希生日快乐”吗？",
+              "icon": path.join(__dirname,'./xiaoxi/love.png')
+            })
+          }
+        },
+        { label: '退出', role: 'quit' }
       ]
     },
     {
       label: '我是谁',
       submenu: [
-        { label: '访问官网' },
-        { label: '关于' }
+        {
+          label: '关于小希', click: () => {
+            shell.openExternal("https://zh.moegirl.org.cn/zh/%E5%B0%8F%E5%B8%8C%E5%B0%8F%E6%A1%83")
+          }
+        },
+        {
+          label: '虚拟次元计划官网', click: () => {
+            shell.openExternal("https://www.vdproject.cn/")
+          }
+        },
+        { type: 'separator' },
+        {
+          label: '该项目开源地址', click: () => {
+            shell.openExternal("https://github.com/zhzhch335/findxiaoxi")
+          }
+        }
       ]
     }
   ];
@@ -86,6 +167,4 @@ app.whenReady().then(() => {
   const menu = Menu.buildFromTemplate(menuBar);
   // 设置一个顶部菜单栏
   Menu.setApplicationMenu(menu);
-
-
-})
+}
